65a7076105ae0dd0598e20b08936a2cad2a0c18d
gabri <gabrielescano.bcn@gmail.com>
2022-03-10 21:47:38 +0100

    Add Dark Layout

---
+++ components/layouts/DarkLayout.js
@@ -0,0 +1,15 @@
+
+export const DarkLayout = ({ children }) => {
+    return (
+        <div style={{
+            backgroundColor: 'rgb(26, 26, 26)',
+            borderRadius: '5px',
+            padding: '10px'
+        }}>
+            <h3>Dark Layout</h3>
+            <div>
+                {children}
+            </div>
+        </div>
+    )
+}
