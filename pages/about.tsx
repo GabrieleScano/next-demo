ffe57e5940ee1e41e16ed1b19d0bee53f98d124d
gabri <gabrielescano.bcn@gmail.com>
2022-03-11 08:02:37 +0100

    Fix any type

--- pages/about.tsx
+++ pages/about.tsx
@@ -18,7 +18,7 @@ export default function AboutPage() {
     )
 }
 
-AboutPage.getLayout = function getLayout(page) {
+AboutPage.getLayout = function getLayout(page: JSX.Element) {
     return (
         <MainLayout>
             <DarkLayout>
